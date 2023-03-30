b68ac898ec7388188869544051f5cc7b4d7bdb26
Florian Walliser <f.walliser@quality-miners.de>
2023-03-30 11:28:55 +0200

    HelloController added

---
+++ src/main/java/de/example/jenkinsmultibranch/JenkinsMultibranchApplication.java
@@ -0,0 +1,13 @@
+package de.example.jenkinsmultibranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMultibranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMultibranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/example/jenkinsmultibranch/JenkinsMultibranchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.example.jenkinsmultibranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMultibranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
